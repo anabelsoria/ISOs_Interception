2458b9cd78595aac88641e8bfb6962efc2a93950
Anabel Soria <anabel@continuum-space.com>
2023-09-21 17:04:31 -0500

    Comment out unnecessary spice files

--- load_lib_paths.m
+++ load_lib_paths.m
@@ -11,16 +11,16 @@ dllDirectory_Path = 'src\Lambert_Russell\matlabInterface\lib\';
 iflag = ivLam_initializeDLL(dllDirectory_Path);
 
 % Load 3BP Rotating Frames
-cspice_furnsh('src\SPICE_files\r3bp_earth-moon.tf')
+% cspice_furnsh('src\SPICE_files\r3bp_earth-moon.tf')
 cspice_furnsh('src\SPICE_files\r3bp_sun-earth.tf')
-cspice_furnsh('src\SPICE_files\rssd0002.tf')
-% Load Solar System Ephemeris and parameters
+% cspice_furnsh('src\SPICE_files\rssd0002.tf')
+% % Load Solar System Ephemeris and parameters
 cspice_furnsh('src\SPICE_files\de440s.bsp')
 cspice_furnsh('src\SPICE_files\naif0012.tls')
-% Eros Comet ephemeris
-cspice_furnsh('src\SPICE_files\2000433.bsp')
-% L2 ephemeris
-cspice_furnsh('src\SPICE_files\L2_de431.bsp')
+% % Eros Comet ephemeris
+% cspice_furnsh('src\SPICE_files\2000433.bsp')
+% % L2 ephemeris
+% cspice_furnsh('src\SPICE_files\L2_de431.bsp')
 
 
 end
